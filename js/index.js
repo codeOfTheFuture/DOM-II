39cd57c5da2c3fa0433bc7381c71da670b0624e8
Jeff Oliver <jeffoliver3384@gmail.com>
2019-05-14 15:55:08 -0400

    WIP: mouseover and mousedown complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+// Your code goes
+
+////// Mouse over /////////
+const headerImg = document.querySelector('.home .intro img');
+
+function blurImg() {
+  headerImg.style.filter = 'blur(5px)';
+}
+
+headerImg.addEventListener('mouseover', blurImg);
+
+////// Scroll Event ///////
+
+// Generate random color
+function getRandomColor() {
+  let hexCode = '0123456789ABCDEF',
+    hexColor = '#';
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += hexCode[Math.floor(Math.random() * 16)];
+  }
+
+  return hexColor;
+}
+
+// Change background color
+function changeBgColor(e) {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+window.addEventListener('scroll', changeBgColor);
